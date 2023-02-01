b20b014332928c3b2621f96880c6b7070f1213a2
Aman Bansal <aman210697@gmail.com>
2023-02-02 01:28:39 +0530

    Refactore Result to Resource and add Loading state

---
+++ app/src/main/java/com/booking/tripsassignment/utils/Resource.kt
@@ -0,0 +1,21 @@
+package com.booking.tripsassignment.utils
+
+/**
+ * A Result implementation class.
+ * It's a sealed class which supports two types: Success And Error.
+ */
+sealed class Resource<out R> {
+    object Loading : Resource<Nothing>()
+    data class Success<out R>(val data: R) : Resource<R>()
+    data class Error(val exception: Exception) : Resource<Nothing>()
+
+    /**
+     * Get the result data if it is Success else null.
+     */
+    fun data(): R? {
+        return when (this) {
+            is Success -> data
+            else -> null
+        }
+    }
+}
